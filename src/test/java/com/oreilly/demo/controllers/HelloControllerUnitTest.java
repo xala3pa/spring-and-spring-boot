a80cac41edf942aa3f35bb722c3923d3c4fe0680
xala3pa <salazar3pa@gmail.com>
2018-08-29 11:13:54 +0200

    Added simple unit test, to test the controller

---
+++ src/test/java/com/oreilly/demo/controllers/HelloControllerUnitTest.java
@@ -0,0 +1,23 @@
+package com.oreilly.demo.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+public class HelloControllerUnitTest {
+
+  @Test
+  public void should_say_hello() {
+    //given
+    HelloController helloController = new HelloController();
+    //and
+    Model model = new BindingAwareModelMap();
+    //when
+    String message = helloController.sayHello("World", model);
+    //then
+    assertEquals("World", model.asMap().get("user"));
+    assertEquals("Hello", message);
+  }
+}
